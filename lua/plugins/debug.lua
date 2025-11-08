a86ff31344b5323c1b00b3a9618e5a35a1b9528b
sachin746 <sachinbhadur9@gmail.com>
2025-11-08 21:06:40 +0530

    added dap

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -62,20 +62,23 @@ return {
 				}
 			end
 
-			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
-			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+			vim.keymap.set("n", "<space>db", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<space>dc", dap.run_to_cursor)
 
 			-- Eval var under cursor
-			vim.keymap.set("n", "<space>?", function()
+			vim.keymap.set("n", "<space>dv", function()
 				require("dapui").eval(nil, { enter = true })
 			end)
 
 			vim.keymap.set("n", "<F1>", dap.continue)
-			vim.keymap.set("n", "<F2>", dap.step_into)
-			vim.keymap.set("n", "<F3>", dap.step_over)
+			vim.keymap.set("n", "<F2>", dap.step_over)
+			vim.keymap.set("n", "<F3>", dap.step_into)
 			vim.keymap.set("n", "<F4>", dap.step_out)
 			vim.keymap.set("n", "<F5>", dap.step_back)
 			vim.keymap.set("n", "<F8>", dap.restart)
+			vim.keymap.set("n", "<leader>dl", function()
+				dap.repl.open()
+			end, { desc = "Toggle DAP REPL" })
 
 			dap.listeners.before.attach.dapui_config = function()
 				ui.open()
