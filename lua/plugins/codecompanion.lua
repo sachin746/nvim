5666b0808ba49a457576f84ba99e228b0c2526fa
sachin746 <sachinbhadur9@gmail.com>
2025-11-11 01:14:18 +0530

    added copilot

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,38 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		-- needed to install additional parsers
+		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+		{ "nvim-lua/plenary.nvim" },
+		-- Test with blink.cmp (delete if not required)
+		{
+			"saghen/blink.cmp",
+			lazy = false,
+			version = "*",
+			opts = {
+				keymap = {
+					preset = "enter",
+					["<S-Tab>"] = { "select_prev", "fallback" },
+					["<Tab>"] = { "select_next", "fallback" },
+				},
+				cmdline = { sources = { "cmdline" } },
+				sources = {
+					default = { "lsp", "path", "buffer", "codecompanion" },
+				},
+			},
+		},
+		-- Test with nvim-cmp
+		-- { "hrsh7th/nvim-cmp" },
+	},
+	opts = {
+		--Refer to: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
+		strategies = {
+			--NOTE: Change the adapter as required
+			chat = { adapter = "copilot" },
+			inline = { adapter = "copilot" },
+		},
+		opts = {
+			log_level = "DEBUG",
+		},
+	},
+}
