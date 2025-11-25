1999b74ff128c7e633cec3f60eb38a3ab9cd3ba1
sachin746 <sachinbhadur9@gmail.com>
2025-11-26 01:39:52 +0530

    added gitsign leetcode noice

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,15 +2,14 @@ return {
 	"lewis6991/gitsigns.nvim",
 	event = "VeryLazy",
 	opts = {
-		current_line_blame = true, -- show blame at end of line
+		current_line_blame = true,
 		current_line_blame_opts = {
 			delay = 300,
 			virt_text_pos = "eol",
 			numhl = true,
-			sign_priority = 15, --- set to a high value to avoid being overridden by other plugins
+			sign_priority = 15,
 		},
-	},
-	require("gitsigns").setup({
+
 		on_attach = function(bufnr)
 			local gitsigns = require("gitsigns")
 
@@ -37,36 +36,11 @@ return {
 				end
 			end)
 
-			-- Actions
-			map("n", "<leader>hs", gitsigns.stage_hunk)
-			map("n", "<leader>hr", gitsigns.reset_hunk)
-
-			map("v", "<leader>hs", function()
-				gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end)
-
-			map("v", "<leader>hr", function()
-				gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end)
-
-			map("n", "<leader>hS", gitsigns.stage_buffer)
-			map("n", "<leader>hR", gitsigns.reset_buffer)
-			map("n", "<leader>hp", gitsigns.preview_hunk)
-			map("n", "<leader>hi", gitsigns.preview_hunk_inline)
-
 			map("n", "<leader>hb", function()
-				gitsigns.blame_line({ full = true })
+				gitsigns.blame({ full = true })
 			end)
 
 			map("n", "<leader>hd", gitsigns.diffthis)
-
-			map("n", "<leader>hD", function()
-				gitsigns.diffthis("~")
-			end)
-
-			-- Toggles
-			map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
-			map("n", "<leader>tw", gitsigns.toggle_word_diff)
 		end,
-	}),
+	},
 }
